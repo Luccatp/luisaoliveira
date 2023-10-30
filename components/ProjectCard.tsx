88bfbdadf9837bb1c21d7b69d7e60bd3a0a68ace
Lucca Paradeda <luccatisserparadeda@gmail.com>
2023-10-30 17:07:56 -0300

    feat: mob responsiveness

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -22,7 +22,7 @@ const ProjectCard: FC<ProjectCardProps> = ({
   link,
 }) => {
   return (
-    <div className="flex justify-between items-center w-full">
+    <div className="flex justify-between flex-col gap-10 sm:flex-row sm:gap-10 items-center w-full">
       <div className={"flex flex-col items-end w-4/5 gap-10"}>
         <BouncingLine />
         <div className={"flex flex-col gap-3 w-full justify-center"}>
@@ -32,7 +32,7 @@ const ProjectCard: FC<ProjectCardProps> = ({
             once={true}
             delay={delay}
             staggerVelocity={0.01}
-            className="m-0 text-3xl ml-8"
+            className="m-0 text-3xl sm:ml-8"
           />
         </div>
         <div className={"flex flex-wrap gap-3"}>
